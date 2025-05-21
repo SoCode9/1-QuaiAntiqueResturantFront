acc76d583666b7cac115b8510521cfb5a9242810
SoCode9 <tennis00015@gmail.com>
2025-05-21 11:11:25 +0200

    Erreur orthographe dans les routes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/galerie", "La galerie", "/pages/galerie.html", []),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconneted"], "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconneted"], "/js/auth/signup.js"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
     new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
