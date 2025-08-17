b42f294891df002a00a966563b4237fc620e314a
Solène <tennis00015@gmail.com>
2025-08-17 11:43:56 +0200

    add ROLE_ADMIN in /carte route

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -10,7 +10,7 @@ export const allRoutes = [
     new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html", ["ROLE_CLIENT", "ROLE_ADMIN"]),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["ROLE_CLIENT"]),
     new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["ROLE_CLIENT"],"/js/reservations/reserver.js"),
-    new Route("/carte", "Notre carte", "/pages/carte.html", ["ROLE_CLIENT"],"/js/carte.js"),
+    new Route("/carte", "Notre carte", "/pages/carte.html", ["ROLE_CLIENT","ROLE_ADMIN"],"/js/carte.js"),
 ];
 
 //Le titre de l'onglet s'affiche comme ceci : Route.titre - websitename
